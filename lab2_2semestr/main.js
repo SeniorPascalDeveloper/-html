e0dbb688e81ca38ee50e513f838e72ac4d29ad28
SeniorPascalDeveloper <toloceksvetlana4@gmail.com>
2026-04-24 17:33:53 +1000

    Add files via upload

---
+++ lab2_2semestr/main.js
@@ -0,0 +1,132 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const width = 600;
+    const height = 600;      
+    const svg = d3.select("svg")
+       .attr("width", width)
+       .attr("height", height);
+
+    const form = document.getElementById("setting");
+    const animatePath = document.getElementById("animatePath");
+    const drawBtn = document.getElementById("drawBtn");
+    const animateToggle = document.getElementById("animateToggle");
+    const hideElements = document.querySelectorAll(".hide");
+    
+    // ← ДОБАВИТЬ эти две строки:
+    const coordinateSpan = document.getElementById("coordinate");
+    const pathSpan = document.getElementById("path");
+
+    document.getElementById("drawBtn").addEventListener("click", function(){
+        draw(form);
+    });
+
+    document.getElementById("clearBtn").addEventListener("click", function(){
+        svg.selectAll('*').remove();
+    });
+
+    document.getElementById("animateBtn").addEventListener("click", function(){
+        runAnimation(form);
+    });
+
+    animateToggle.addEventListener("change", function() {
+        hideElements.forEach(el => {
+            if (this.checked) {
+                el.classList.remove("hide");
+            } else {
+                el.classList.add("hide");
+            }
+        });
+
+        if (this.checked) {
+            drawBtn.classList.add("hide");
+        } else {
+            drawBtn.classList.remove("hide");
+        }
+
+        if (this.checked) {
+        } else {
+            pathSpan.classList.add("hideSecond");
+            coordinateSpan.classList.remove("hideSecond");
+            animatePath.checked = false;
+        }
+    });
+    
+    animatePath.addEventListener("change", function() {
+        if (this.checked) {
+            coordinateSpan.classList.add("hideSecond");
+            pathSpan.classList.remove("hideSecond");
+        } else {
+            coordinateSpan.classList.remove("hideSecond");
+            pathSpan.classList.add("hideSecond");
+        }
+    });
+});
+
+const draw = (dataForm) => {
+    const svg = d3.select("svg");
+    let pict = drawSmile(svg);
+    let scaleX = dataForm.scaleX.value || 1;
+    let scaleY = dataForm.scaleY.value || 1;
+    
+    pict.attr("transform", `translate(${dataForm.cx.value},
+                                      ${dataForm.cy.value}) 
+                            scale(${scaleX}, ${scaleY})
+                            rotate(${dataForm.angle.value})`);
+};
+
+
+
+const runAnimation = (dataForm) => {
+
+    const svg = d3.select("svg");
+    let pict = drawSmile(svg);
+
+    if (!animatePath.checked){
+
+        
+    const x1 = +dataForm.cx.value;
+    const y1 = +dataForm.cy.value;
+    const angle1 = +dataForm.angle.value || 0;
+    const scaleX1 = +dataForm.scaleX.value || 1;
+    const scaleY1 = +dataForm.scaleY.value || 1;
+
+    const x2 = +dataForm.cx2.value;
+    const y2 = +dataForm.cy2.value;
+    const angle2 = +dataForm.angle2.value || 0;
+    const scaleX2 = +dataForm.scaleX2.value || 1;
+    const scaleY2 = +dataForm.scaleY2.value || 1;
+
+    let easeType;
+    switch (dataForm.easeType.value) {
+        case "elastic": easeType = d3.easeElastic; break;
+        case "bounce":  easeType = d3.easeBounce;  break;
+        default:        easeType = d3.easeLinear;
+    }
+
+    animatePath.checked = false;
+
+    pict.attr("transform", `translate(${x1}, ${y1}) scale(${scaleX1}, ${scaleY1}) rotate(${angle1})`);
+
+    pict.transition()
+        .duration(6000)
+        .ease(easeType)
+        .attr("transform", `translate(${x2}, ${y2}) scale(${scaleX2}, ${scaleY2}) rotate(${angle2})`);
+        
+    } else{
+        
+    const pathType = +document.getElementById("pathType").value;
+
+    let path = drawPath(pathType);
+
+    let easeType;
+    switch (dataForm.easeType.value) {
+        case "elastic": easeType = d3.easeElastic; break;
+        case "bounce":  easeType = d3.easeBounce;  break;
+        default:        easeType = d3.easeLinear;
+    }
+
+    pict.transition()
+        .duration(6000)
+        .ease(easeType)
+        .attrTween('transform', translateAlong(path.node()));
+    }
+};
